790c22f1cf24d400dbae1816f608bdeb50d9ad46
LinRN <44719485+Linrnn@users.noreply.github.com>
2024-12-30 22:39:43 +0800

    chore：【Fixed】完善Random完整实例

--- Scripts/RandomSample.cs
+++ Scripts/RandomSample.cs
@@ -16,6 +16,7 @@ internal sealed class RandomSample : MonoBehaviour
         System,
         Unity,
         Array,
+        MersenneTwister,
     }
 
     [Serializable]
@@ -28,10 +29,10 @@ internal sealed class RandomSample : MonoBehaviour
         internal void Check(T item)
         {
             if (item.CompareTo(Min) < 0)
-                LogRelay.Error($"[Random] item:{item} < {Min}");
+                LogRelay.Error($"[Random] {typeof(T).Name} item:{item} < {Min}");
 
             if (item.CompareTo(Max) >= 0)
-                LogRelay.Error($"[Random] item:{item} >= {Max}");
+                LogRelay.Error($"[Random] {typeof(T).Name} item:{item} >= {Max}");
 
             _value.Add(item);
         }
@@ -41,7 +42,7 @@ internal sealed class RandomSample : MonoBehaviour
 
     [SerializeField] private RandomType _randomType;
     [SerializeField] private int _seed;
-    [SerializeField] private byte _times;
+    [SerializeField] private ulong _times;
 
     [Space] [SerializeField] private MinMax<sbyte> _sbyte;
     [SerializeField] private MinMax<byte> _byte;
@@ -58,44 +59,45 @@ internal sealed class RandomSample : MonoBehaviour
         {
             case RandomType.System: RandomProxy.Inject(new SystemRandom(_seed)); break;
             case RandomType.Unity: RandomProxy.Inject(new UnityRandom(_seed)); break;
-            case RandomType.Array: RandomProxy.Inject(new ArrayRandom()); break;
+            case RandomType.Array: RandomProxy.Inject(new ArrayRandom(_seed)); break;
+            case RandomType.MersenneTwister: RandomProxy.Inject(new MersenneTwisterRandom(_seed)); break;
         }
     }
     private void Update()
     {
-        Profiler.BeginSample($"RandomSample.Update RandomType:{_randomType}");
+        Profiler.BeginSample("RandomSample.Update");
 
         #region loop random
         _sbyte.Clean();
-        for (int i = 0; i < _times; ++i)
+        for (ulong i = 0; i < _times; ++i)
             _sbyte.Check(RandomRelay.Get(_sbyte.Min, _sbyte.Max));
 
         _byte.Clean();
-        for (int i = 0; i < _times; ++i)
+        for (ulong i = 0; i < _times; ++i)
             _byte.Check(RandomRelay.Get(_byte.Min, _byte.Max));
 
         _short.Clean();
-        for (int i = 0; i < _times; ++i)
+        for (ulong i = 0; i < _times; ++i)
             _short.Check(RandomRelay.Get(_short.Min, _short.Max));
 
         _ushort.Clean();
-        for (int i = 0; i < _times; ++i)
+        for (ulong i = 0; i < _times; ++i)
             _ushort.Check(RandomRelay.Get(_ushort.Min, _ushort.Max));
 
         _int.Clean();
-        for (int i = 0; i < _times; ++i)
+        for (ulong i = 0; i < _times; ++i)
             _int.Check(RandomRelay.Get(_int.Min, _int.Max));
 
         _uint.Clean();
-        for (int i = 0; i < _times; ++i)
+        for (ulong i = 0; i < _times; ++i)
             _uint.Check(RandomRelay.Get(_uint.Min, _uint.Max));
 
         _long.Clean();
-        for (int i = 0; i < _times; ++i)
+        for (ulong i = 0; i < _times; ++i)
             _long.Check(RandomRelay.Get(_long.Min, _long.Max));
 
         _ulong.Clean();
-        for (int i = 0; i < _times; ++i)
+        for (ulong i = 0; i < _times; ++i)
             _ulong.Check(RandomRelay.Get(_ulong.Min, _ulong.Max));
         #endregion
 
