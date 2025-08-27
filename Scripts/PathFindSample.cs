79427869d99780c5caf916989269a0692172d81a
LinRN <44719485+Linrnn@users.noreply.github.com>
2025-08-27 22:04:29 +0800

    chore：【PathFind】补充枚举

--- Scripts/PathFindSample.cs
+++ Scripts/PathFindSample.cs
@@ -5,6 +5,7 @@ using EeveeEditor.PathFind;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using UnityEngine;
 using CollSize = System.SByte;
 using Ground = System.Byte;
@@ -18,24 +19,32 @@ internal sealed class PathFindSample : MonoBehaviour
     #region 类型
     private enum GroundType : Ground
     {
-        // todo eevee
+        None = 0,
+        Any = 1,
+        Walk = 2,
+        Fly = 4,
+        Cons = 8,
+        Peon = 16,
+        Blight = 32,
+        Water = 64,
+        Amp = 128,
     }
 
     private enum MoveType : MoveFunc
     {
-        Fly,
-        Marin,
-        Water,
-        Foot,
+        Fly = GroundType.Fly,
+        Marin = GroundType.Amp,
+        Water = GroundType.Water | GroundType.Amp,
+        Foot = GroundType.Walk | GroundType.Amp,
     }
 
     private enum CollType : CollSize
     {
-        S0,
-        S1,
-        S2,
-        S3,
-        S4,
+        _0,
+        _1,
+        _2,
+        _3,
+        _4,
     }
 
     private sealed class SamplePathFindDrawProxy : IPathFindDrawProxy
@@ -46,7 +55,7 @@ internal sealed class PathFindSample : MonoBehaviour
         public PathFindComponent Component => _sample._component;
         public Vector2 MinBoundary => _sample._minBoundary;
         public float GridSize => _sample._gridSize;
-        public bool ValidColl(CollSize value) => value is >= (CollSize)CollType.S1 and <= (CollSize)CollType.S4;
+        public bool ValidColl(CollSize value) => value is >= (CollSize)CollType._1 and <= (CollSize)CollType._4;
         // todo eevee
         public Vector2Int? GetCurrentPoint(int index) => throw new NotImplementedException();
         public Vector2? GetMoveDirection(int index) => throw new NotImplementedException();
@@ -54,30 +63,45 @@ internal sealed class PathFindSample : MonoBehaviour
 
     private sealed class SamplePathFindCollisionGetter : IPathFindCollisionGetter
     {
-        public CollSize GetNull() => (CollSize)CollType.S0;
+        public CollSize GetNull() => (CollSize)CollType._0;
         public CollSize GetMax(IList<CollSize> collisions) => collisions.GetMax();
         public PathFindPeek Get(CollSize coll) => coll switch
         {
-            (CollSize)CollType.S1 => default,
-            (CollSize)CollType.S2 => new PathFindPeek(-1, -1, 0, 0),
-            (CollSize)CollType.S3 => new PathFindPeek(-1, -1, 1, 1),
-            (CollSize)CollType.S4 => new PathFindPeek(-2, -2, 1, 1),
+            (CollSize)CollType._1 => default,
+            (CollSize)CollType._2 => new PathFindPeek(-1, -1, 0, 0),
+            (CollSize)CollType._3 => new PathFindPeek(-1, -1, 1, 1),
+            (CollSize)CollType._4 => new PathFindPeek(-2, -2, 1, 1),
             _ => throw new IndexOutOfRangeException($"不合法的碰撞尺寸：{coll}"),
         };
         public PathFindPeek Get(int x, int y, CollSize coll) => coll switch
         {
-            (CollSize)CollType.S1 => new PathFindPeek(x, y, x, y),
-            (CollSize)CollType.S2 => new PathFindPeek(x - 1, y - 1, x, y),
-            (CollSize)CollType.S3 => new PathFindPeek(x - 1, y - 1, x + 1, y + 1),
-            (CollSize)CollType.S4 => new PathFindPeek(x - 2, y - 2, x + 1, y + 1),
+            (CollSize)CollType._1 => new PathFindPeek(x, y, x, y),
+            (CollSize)CollType._2 => new PathFindPeek(x - 1, y - 1, x, y),
+            (CollSize)CollType._3 => new PathFindPeek(x - 1, y - 1, x + 1, y + 1),
+            (CollSize)CollType._4 => new PathFindPeek(x - 2, y - 2, x + 1, y + 1),
             _ => throw new IndexOutOfRangeException($"不合法的碰撞尺寸：{coll}"),
         };
     }
 
     private sealed class SamplePathPathFindObjectPoolGetter : IPathFindObjectPoolGetter
     {
+        private readonly object _listPoolLock = new();
+        private readonly object _mapPoolLock = new();
+
         public List<T> ListAlloc<T>() => ListPool.Alloc<T>();
-        public List<T> ListAlloc<T>(int capacity) => ListPool.Alloc<T>();
+        public List<T> ListAlloc<T>(bool fromThread)
+        {
+            if (fromThread)
+                lock (_listPoolLock)
+                    return ListPool.Alloc<T>();
+            return ListPool.Alloc<T>();
+        }
+        public List<T> ListAlloc<T>(int capacity)
+        {
+            var collection = ListPool.Alloc<T>();
+            collection.Capacity = capacity;
+            return collection;
+        }
         public void Alloc<T>(ref List<T> collection) => ListPool.Alloc(ref collection);
         public void Release<T>(List<T> collection) => collection.Release2Pool();
         public void Release<T>(ref List<T> collection) => ListPool.Release(ref collection);
@@ -88,9 +112,22 @@ internal sealed class PathFindSample : MonoBehaviour
         public void Alloc<T>(ref HashSet<T> collection) => HashSetPool.Alloc(ref collection);
         public void Release<T>(ref HashSet<T> collection) => HashSetPool.Release(ref collection);
 
-        public Dictionary<TKey, TValue> MapAlloc<TKey, TValue>() => DictionaryPool.Alloc<TKey, TValue>();
+        public Dictionary<TKey, TValue> MapAlloc<TKey, TValue>(bool fromThread)
+        {
+            if (fromThread)
+                lock (_mapPoolLock)
+                    return DictionaryPool.Alloc<TKey, TValue>();
+            return DictionaryPool.Alloc<TKey, TValue>();
+        }
         public void Alloc<TKey, TValue>(ref Dictionary<TKey, TValue> collection) => DictionaryPool.Alloc(ref collection);
-        public void Release<TKey, TValue>(Dictionary<TKey, TValue> collection) => collection.Release2Pool();
+        public void Release<TKey, TValue>(Dictionary<TKey, TValue> collection, bool fromThread)
+        {
+            if (fromThread)
+                lock (_mapPoolLock)
+                    collection.Release2Pool();
+            else
+                collection.Release2Pool();
+        }
         public void Release<TKey, TValue>(ref Dictionary<TKey, TValue> collection) => DictionaryPool.Release(ref collection);
     }
     #endregion
@@ -109,9 +146,9 @@ internal sealed class PathFindSample : MonoBehaviour
     private void OnEnable()
     {
         const int offset = 2;
-        string[] lines = _map.text.Split('\n');
-        int mapWidth = lines[0].Length / offset;
-        int mapHeight = lines.Length;
+        string[] lines = _map.text.Split('\n', '\r').Where(line => !string.IsNullOrWhiteSpace(line)).ToArray();
+        int mapWidth = lines.Length;
+        int mapHeight = lines[0].Length / offset;
 
         Ground[,] nodes = new Ground[mapWidth, mapHeight];
         MoveFunc[][] moveTypeGroups =
@@ -129,10 +166,10 @@ internal sealed class PathFindSample : MonoBehaviour
         };
         CollSize[] collTypes =
         {
-            (CollSize)CollType.S1,
-            (CollSize)CollType.S2,
-            (CollSize)CollType.S3,
-            (CollSize)CollType.S4,
+            (CollSize)CollType._1,
+            (CollSize)CollType._2,
+            (CollSize)CollType._3,
+            (CollSize)CollType._4,
         };
         var getters = new PathFindGetters(new SamplePathFindCollisionGetter(), new SamplePathPathFindObjectPoolGetter());
 
